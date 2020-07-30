c9e1c6ade2ceb9dc7d96115a533833fc2da03a73
Shuvankar Paul <shuvoenr@gmail.com>
2020-07-31 00:19:51 +0600

    IP whitelisting Feature

--- Bash-Script/elasticsearch-interactive.sh
+++ Bash-Script/elasticsearch-interactive.sh
@@ -3,6 +3,7 @@
 # log path you can view later what happen
 LOG_FULL_PATH=/var/log/elasicsearch-setup-enviroment.log
 LOG_DIR_PATH=/var/log/
+ELASTIC_CONFIG_PATH=/etc/elasticsearch/elasticsearch.yml
 
 # Temporary colors of tput
 BLACK="$(tput setaf 0)"
@@ -55,6 +56,46 @@ installJavaDefault() {
     java -version
 }
 
+#Install Elasticsearch Dynamically
+installElasticsearch() {
+    printf  "${GREEN} Installing... Elasticseach... ${NORMAL}\\n"
+    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+    echo "deb https://artifacts.elastic.co/packages/${1}.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-${1}.x.list
+    printf "deb https://artifacts.elastic.co/packages/${1}.x/apt stable main"
+    sudo apt update --assume-yes >> "$LOG_FULL_PATH"
+    sudo apt install -y elasticsearch --assume-yes >> "$LOG_FULL_PATH"
+    sudo /bin/systemctl daemon-reload
+    sudo /bin/systemctl enable elasticsearch.service
+    sudo systemctl start elasticsearch.service
+    # sudo systemctl status elasticsearch.service
+    echo "Waiting for ElasticSearch to boot up..."
+    sleep 20
+    curl -XGET 'localhost:9200/?pretty'
+    printf  "${GREEN} Do you want to whitelist IP address?${NORMAL}\\n"
+    printf  "${YELLOW} All IP Address Type 1${NORMAL}\\n"
+    printf  "${YELLOW} Specific IP Address Type 2${NORMAL}\\n"
+    printf  "${YELLOW} Use as Localhost Type 3${NORMAL}\\n"
+    read -p ": " whitelistIp
+    if [ "$whitelistIp" =  1 ]
+    then
+        echo  "network.host: 0.0.0.0" >> "$ELASTIC_CONFIG_PATH"
+        echo  "discovery.seed_hosts: []" >> "$ELASTIC_CONFIG_PATH"
+    elif [ "$whitelistIp" =  2 ]
+    then
+        printf  "${YELLOW} Enter the Public IP Address from where you going to access?: ${NORMAL}\\n"
+        read -p ": " ipAddressWhitelist
+        echo  "network.host: ${ipAddressWhitelist}" >> "$ELASTIC_CONFIG_PATH"
+        echo  "discovery.seed_hosts: []" >> "$ELASTIC_CONFIG_PATH"
+    else
+        echo "We will use it as localhost service"
+    fi
+
+    #reloading the things
+    sudo /bin/systemctl daemon-reload
+    sudo systemctl restart elasticsearch.service
+    curl -XGET 'localhost:9200/?pretty'
+}
+
 printf  "${YELLOW} we are starting the process, please do response if something is asked to you. ${NORMAL}\\n"
 printf  "${GREEN} Updatig.... ${NORMAL}\\n"
 # Update assume yes command
@@ -76,3 +117,22 @@ then
 else
    installJavaDefault
 fi
+
+#Installig the Elasticsearch
+printf  "${YELLOW} Elasticsearch Installing.... ${NORMAL}\\n"
+read -p "Which Elasticsearch Version you want to setup [only 5, 6, 7] : " elasicsearchVersion
+
+if [ "$elasicsearchVersion" =  5 ]
+then
+    installElasticsearch 5
+elif [ "$elasicsearchVersion" =  6 ]
+then
+   installElasticsearch 6
+elif [ "$elasicsearchVersion" =  7 ]
+then
+   installElasticsearch 7
+else
+    echo "Skip the Elasticserch Install.."
+fi
+
+printf  "${YELLOW} Installing.... Done Successfully.... ${NORMAL}\\n"
