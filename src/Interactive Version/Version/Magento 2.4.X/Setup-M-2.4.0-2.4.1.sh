169094f074577806a7ba4609a04f8142214d6de3
Suvankar Paul <shuvoenr@gmail.com>
2020-12-25 00:22:03 +0600

    Elastic Search Install Added

--- src/Interactive Version/Version/Magento 2.4.X/Setup-M-2.4.0-2.4.1.sh
+++ src/Interactive Version/Version/Magento 2.4.X/Setup-M-2.4.0-2.4.1.sh
@@ -33,8 +33,6 @@ installComposer() {
   echo "Do you want to install composer? [Y/N] : "
   read -r wantComposerInstall
   if [ "$wantComposerInstall" = "Y" ] || [ "$wantComposerInstall" = "y" ]; then
-    # -- Install composer dependent package
-    sudo apt install wget php-cli php-zip unzip --assume-yes
     wget -O composer-setup.php https://getcomposer.org/installer
     sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
     composer -V
@@ -43,15 +41,74 @@ installComposer() {
   fi
 }
 
+# -- Install PHP
+installPHP() {
+  sudo add-apt-repository ppa:ondrej/php
+  sudo apt-get update --assume-yes
+  sudo apt-get install --assume-yes php7.4-{common,fpm,cli,pdo,mysql,opcache,xml,gd,mysql,intl,mbstring,bcmath,json,iconv,soap,ctype,curl,dom,intl,xsl,zip,sockets}
+}
+
+# -- Install Elastic Search
+installElasticSearch () {
+  echo -p "Want to Install ElasticSearch? [Y/N] : "
+  read -r wantElasticSearchInstall
+  if [ "$wantElasticSearchInstall" = "Y" ] || [ "$wantElasticSearchInstall" = "y" ]; then
+    printf "Installing ElasticSearch...\n"
+    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+    echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+    printf "deb https://artifacts.elastic.co/packages/7.x/apt stable main"
+    sudo apt update --assume-yes
+    sudo apt install -y elasticsearch --assume-yes
+    sudo /bin/systemctl daemon-reload
+    sudo /bin/systemctl enable elasticsearch.service
+    sudo systemctl start elasticsearch.service
+    # sudo systemctl status elasticsearch.service
+    echo "Waiting for ElasticSearch to boot up..."
+    sleep 20
+    curl -XGET 'localhost:9200/?pretty'
+    printf  "Do you want to whitelist IP address?\n"
+    printf  "All IP Address Type, 1\n"
+    printf  "Specific IP Address, Type 2\n"
+    printf  "Use as Localhost Type, 3\n"
+    printf ": "
+    read -r whitelistIp
+    if [ "$whitelistIp" =  1 ]
+    then
+        sudo echo "network.host: 0.0.0.0"
+        sudo echo "discovery.seed_hosts: []"
+    elif [ "$whitelistIp" =  2 ]
+    then
+        printf  "Enter the Public IP Address from where you going to access? : \n"
+        read -r ipAddressWhitelist
+        sudo echo "network.host: ${ipAddressWhitelist}"
+        sudo echo "discovery.seed_hosts: []"
+    else
+        echo "We will use it as localhost service"
+    fi
+
+    #reloading the things
+    sudo /bin/systemctl daemon-reload
+    sudo systemctl restart elasticsearch.service
+    curl -XGET 'localhost:9200/?pretty'
+  fi
+}
+
 printf  "We are starting the process, Cooperate by giving you choice, if you asked for anything.... \n"
 
 # Update assume yes command
 sudo apt-get update --assume-yes
 sudo apt-get upgrade --assume-yes
+sudo apt-get install curl zip unzip --assume-yes
 
 # Swap Memory
 swapCreate
 
 # Composer Install
 installComposer
+
+# install PHP
+installPHP
+
+# install Elastic Search
+installElasticSearch
 
